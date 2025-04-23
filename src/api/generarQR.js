f28589b0a0154f45066dac5a3df1ea2b03d9be3a
Maxim <107987427+maxder1902@users.noreply.github.com>
2025-04-23 06:45:22 +0200

    Update generarQR.js

--- src/api/generarQR.js
+++ src/api/generarQR.js
@@ -1,26 +1,17 @@
-// src/api/generarQR.js
-export async function generarQR(formData) {
-    try {
-      // Realiza la solicitud POST al servidor Express para generar los códigos QR
-      const response = await fetch('/api/generarQR', {
-        method: 'POST',
-        body: formData,
-      });
-  
-      // Verifica si la respuesta es exitosa
-      if (!response.ok) {
-        throw new Error('Error al generar los QR');
-      }
-  
-      // Si la respuesta es exitosa, obtén el archivo generado como Blob
-      const blob = await response.blob();
-      const url = window.URL.createObjectURL(blob);
-  
-      // Devuelve la URL del archivo para que el cliente pueda descargarlo
-      return url;
-    } catch (error) {
-      console.error('Error al enviar el archivo:', error);
-      throw error;
-    }
-  }
-  
+const API_URL = import.meta.env.VITE_API_URL;
+
+const enviarArchivo = async (archivo) => {
+  const formData = new FormData();
+  formData.append('archivo', archivo);
+
+  const res = await fetch(`${API_URL}/api/generarQR`, {
+    method: 'POST',
+    body: formData
+  });
+
+  if (!res.ok) throw new Error('Error al generar QR');
+
+  const blob = await res.blob();
+  const url = window.URL.createObjectURL(blob);
+  window.open(url);
+};
